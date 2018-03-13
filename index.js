3a0f4943d42805cc51587aa65a1883ec617b4fda
nblxanueb <a.e.pykhantsev@gmail.com>
2018-03-13 21:57:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -21,4 +21,10 @@ function sayHiToGrandma(string) {
   if((string) === lowercase){
     return "I can't hear you!"
   }
+  else if((string)===uppercase){
+    return "YES INDEED!"
+  }
+  else if ((string)==="I love you, Grandma."{
+    return "I love you, too."
+  }
 }
